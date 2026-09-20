0d76fa01d99a5c12285b7ce032294711a8343ee4
allaroundchitown-creator <allaroundchitown@gmail.com>
2026-09-20 17:15:09 -0500

    Add 2, 4, and 6 hour packages

--- book.js
+++ book.js
@@ -2,6 +2,7 @@ const bookingForm = document.getElementById('bookingForm');
 const bookingStatus = document.getElementById('bookingStatus');
 const phoneInput = bookingForm.elements.phone;
 const dateInput = bookingForm.elements.eventDate;
+const rentalLengthInput = bookingForm.elements.rentalLength;
 const fakePhoneNumbers = new Set([
   '0000000000', '1111111111', '1234567890', '0123456789',
   '9876543210', '0987654321', '1231231234', '5555555555'
@@ -10,6 +11,11 @@ const fakePhoneNumbers = new Set([
 const today = new Date();
 today.setMinutes(today.getMinutes() - today.getTimezoneOffset());
 dateInput.min = today.toISOString().split('T')[0];
+
+const requestedPackage = new URLSearchParams(window.location.search).get('package');
+if (['2', '4', '6'].includes(requestedPackage)) {
+  rentalLengthInput.value = `${requestedPackage} hours`;
+}
 
 const validatePhone = () => {
   const digits = phoneInput.value.replace(/\D/g, '');
